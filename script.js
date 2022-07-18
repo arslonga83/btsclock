92f309a5f8d20499ec481062b3811133a19c908e
arslonga83 <jlwood83@gmail.com>
2022-07-17 21:26:44 -0700

    build javascript local time, updates every second and uses 2 digit numbers

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+
+
+const display = document.getElementById("localtime");
+clock();
+
+
+function clock() {
 const date = new Date();
-const utcTime = date.getHours() + " : " + date.getMinutes() + " : " + date.getSeconds();
-
-const display = document.getElementById("utctime");
-
-display.textContent = "Current UTC time is " + utcTime;
-
+const h = date.getHours().toLocaleString('en-US', {
+    minimumIntegerDigits: 2, 
+    useGrouping:false
+});
+const m = date.getMinutes().toLocaleString('en-US', {
+    minimumIntegerDigits: 2, 
+    useGrouping:false
+});
+const s = date.getSeconds().toLocaleString('en-US', {
+    minimumIntegerDigits: 2, 
+    useGrouping:false
+});
+const localTime = h + " : " + m + " : " + s;
+display.textContent = "Current local time is " + localTime;
+setTimeout(clock, 1000)
+}

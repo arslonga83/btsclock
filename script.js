fb3a1f5e4665ee90978c6456165a1885f455f263
arslonga83 <jlwood83@gmail.com>
2022-07-17 23:18:00 -0700

    placeholder ktime script

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const display = document.getElementById("localtime");
 clock();
+const kdisplay = document.getElementById("koreantime");
+koreantime();
 
 
 function clock() {
@@ -21,4 +23,29 @@ const s = date.getSeconds().toLocaleString('en-US', {
 const localTime = h + " : " + m + " : " + s;
 display.textContent = localTime;
 setTimeout(clock, 1000)
+}
+
+
+function koreantime() {
+const date = new Date();
+let kh = date.getHours().toLocaleString('en-US', {
+    minimumIntegerDigits: 2, 
+    useGrouping:false
+});
+//kh += 16;
+//if (kh >= 24) {
+//    kh -= 24;
+//}
+const km = date.getMinutes().toLocaleString('en-US', {
+    minimumIntegerDigits: 2, 
+    useGrouping:false
+});
+const ks = date.getSeconds().toLocaleString('en-US', {
+    minimumIntegerDigits: 2, 
+    useGrouping:false
+});
+const koreanTime = kh + " : " + km + " : " + ks;
+kdisplay.textContent = koreanTime;
+console.log(kh, km, ks);
+setTimeout(koreantime, 1000)
 }

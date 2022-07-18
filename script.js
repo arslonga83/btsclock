62baf77c79184bdc4e0cf5dc79466827493422a5
arslonga83 <jlwood83@gmail.com>
2022-07-18 15:52:10 -0700

    add js to adjust ktime 16 hours ahead and include day of the week for both. style changes to position items evenly in each clock and push media player down offscreen

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
 
 const display = document.getElementById("localtime");
+const display2 = document.getElementById("day");
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 clock();
 const kdisplay = document.getElementById("koreantime");
+const kdisplay2 = document.getElementById("koreanday");
 koreantime();
 
 
@@ -20,23 +23,33 @@ const s = date.getSeconds().toLocaleString('en-US', {
     minimumIntegerDigits: 2, 
     useGrouping:false
 });
+const d = days[date.getDay()];
 const localTime = h + " : " + m + " : " + s;
 display.textContent = localTime;
-setTimeout(clock, 1000)
+display2.textContent = d;
+setTimeout(clock, 1000) //reruns the script once per second
 }
 
 
 function koreantime() {
 const date = new Date();
-let kh = date.getHours().toLocaleString('en-US', {
+
+let i = date.getDay(); //sets day to a variable so we can push it ahead when needed
+let d = days[i];
+
+let h = date.getHours().toLocaleString('en-US', {
     minimumIntegerDigits: 2, 
     useGrouping:false
 });
-//kh += 16;
-//if (kh >= 24) {
-//    kh -= 24;
-//}
-const km = date.getMinutes().toLocaleString('en-US', {
+let kh = +h + 16; //add 16 hours to local time...would need to adjust for daylight savings
+if (kh >= 8) {
+    d = days[i+1];
+}
+if (+kh >= 24) {
+    kh -= 24;
+}
+
+const km = date.getMinutes().toLocaleString('en-US', { //minutes and seconds don't need adjusting
     minimumIntegerDigits: 2, 
     useGrouping:false
 });
@@ -44,8 +57,9 @@ const ks = date.getSeconds().toLocaleString('en-US', {
     minimumIntegerDigits: 2, 
     useGrouping:false
 });
+
 const koreanTime = kh + " : " + km + " : " + ks;
 kdisplay.textContent = koreanTime;
-console.log(kh, km, ks);
+kdisplay2.textContent = d;
 setTimeout(koreantime, 1000)
 }
